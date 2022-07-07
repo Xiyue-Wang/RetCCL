0f479882e276cff249ca51e550c4fa040bb12919
Xiyue-Wang <61115068+Xiyue-Wang@users.noreply.github.com>
2022-07-07 20:16:42 +0800

    Update resnet_lincls.py

--- resnet_lincls.py
+++ resnet_lincls.py
@@ -4,6 +4,14 @@ import os
 model = torchvision.models.__dict__['resnet50'](pretrained=False)
 state_dict = torch.load(r'./best_ckpt.pth')
 
+
+for name, param in model.named_parameters():
+    if name not in ['fc.weight', 'fc.bias']:
+        param.requires_grad = False
+# init the fc layer
+model.fc.weight.data.normal_(mean=0.0, std=0.01)
+model.fc.bias.data.zero_()
+
 for k in list(state_dict.keys()):
     # retain only encoder_q up to before the embedding layer
     if k.startswith('encoder_q') and not k.startswith('encoder_q.fc'):
